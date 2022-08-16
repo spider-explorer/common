eba6bf1761fc0426bba0998eb492c1aae1adcab9
JavaCommons Technologies <javacommons@gmail.com>
2022-08-16 20:55:17 +0900

    .

--- str/cbor-api.mjs
+++ str/cbor-api.mjs
@@ -1,16 +1,5 @@
 import { encode, decode } from "https://deno.land/x/cbor@v1.3.3/index.js";
 
-export function VARIANT_GET_ARGS(__args__) {
-    const argsView = new Deno.UnsafePointerView(__args__);
-    console.log(argsView.getCString());
-    let argsBase64 = argsView.getCString();
-    let argsCbor = base64ToUint8Array(argsBase64);
-    //console.log(resultCbor);
-    //console.log(typeof resultCbor);
-    let args = decode(argsCbor);
-    return args;
-}
-
 export class CborLibrary {
     constructor(libPath, methodList) {
         this.libPath = libPath;
@@ -22,28 +11,18 @@ export class CborLibrary {
         this.dylib = Deno.dlopen(libPath, methodDict);
     }
     call(methodName, args) {
-        //console.log(args);
         let argsCbor = encode(args);
-        //console.log(argsCbor);
-        //console.log(typeof argsCbor);
         const argsCborBase64 = uint8ArrayToBase64(argsCbor);
-        //console.log(argsCborBase64);
-        //console.log(typeof argsCborBase64);
-        const string = new Uint8Array([
+        const argsArray = new Uint8Array([
             ...new TextEncoder().encode(argsCborBase64),
             0,
         ]);
-        const stringPtr = Deno.UnsafePointer.of(string);
-        const resultPtr = this.dylib.symbols[methodName](stringPtr);
-        //console.dir(resultPtr);
-        const resultView = new Deno.UnsafePointerView(resultPtr);
-        //console.log(resultView.getCString());
+        const __args__ = Deno.UnsafePointer.of(argsArray);
+        const __result__ = this.dylib.symbols[methodName](__args__);
+        const resultView = new Deno.UnsafePointerView(__result__);
         let resultBase64 = resultView.getCString();
         let resultCbor = base64ToUint8Array(resultBase64);
-        //console.log(resultCbor);
-        //console.log(typeof resultCbor);
         let result = decode(resultCbor);
-        //console.log(result);
         return result;
     }
 }
@@ -64,29 +43,19 @@ export class CborCallback {
                 that.argsBase64 = that.argsView.getCString();
                 that.argsCbor = base64ToUint8Array(that.argsBase64);
                 that.args = decode(that.argsCbor);
-                console.log("(A):" + that.args);
-                console.log("(A2):" + typeof that.args);
-                console.log("(A3):" + typeof f);
                 that.result = that.f(that.args);
-                console.log("(A4):"+ that.result);
                 that.resultCbor = encode(that.result);
                 that.resultCborBase64 = uint8ArrayToBase64(that.resultCbor);
-                that.resultString = new Uint8Array([
+                that.resultArray = new Uint8Array([
                     ...new TextEncoder().encode(that.resultCborBase64),
                     0,
                 ]);
-                that.__result__ = Deno.UnsafePointer.of(that.resultString);
-                console.log("(C)");
+                that.__result__ = Deno.UnsafePointer.of(that.resultArray);
                 return that.__result__;
             },
         );
         that.pointer = String(that.callback.pointer);
     }
-    /*
-    pointer() {
-        return String(this.callback.pointer);
-    }
-    */
     close() {
         this.callback.close();
     }
